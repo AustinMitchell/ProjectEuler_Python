bcb0e7989be192ba80e422c57e1728b8f215ebf3
Austin Mitchell <bigpapamitchell@gmail.com>
2014-12-09 15:10:14 -0500

    Rename p1.py to p001.py

---
+++ p001.py
@@ -0,0 +1,3 @@
+from __future__ import division
+
+print sum([i for i in range(1, 1000) if i%3 == 0 or i%5 == 0])
--- p1.py
+++
@@ -1,3 +0,0 @@
-from __future__ import division
-
-print sum([i for i in range(1, 1000) if i%3 == 0 or i%5 == 0])
